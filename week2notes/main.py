fea095488692cd3f6d6a28a93d7359a4a6d40e56
Jay Abegglen <jay.abegglen@pcc.edu>
2023-10-03 20:00:23 -0700

    message

---
+++ week2notes/main.py
@@ -0,0 +1,53 @@
+#******************************************************************************
+# Author:           Jay Abegglen, Robert Grays
+# Lab:              2
+# Date:             10/03/2023
+# Description:      This program takes in current temperature and pressure readings
+#                   from user, does some calculations, then returns average readings
+# Input:            (What the program asks for, and the data type, e.g. string)
+# Output:           (Summary of messages displayed by the program)
+# Sources:          Lab 1 specifications
+#                   Module 2
+#******************************************************************************
+
+from datetime import datetime
+
+
+def get_temperature():
+    temp = float(input("What is the current temperature? "))
+    return temp
+
+
+def get_temperature_final():
+    temperature_final = float(input("What is the final temperature? "))
+    return temperature_final
+
+
+def get_minutes():
+    minutes = float(input("How many minutes passed? "))
+    return minutes
+
+
+def calc_average(temperature_final, temperature_initial, minutes):
+    temperature_final += temperature_initial
+    temperature_final /= minutes
+    return round(temperature_final)
+
+
+def print_all(temperature_final, temperature_initial, average_temperature):
+    print("Current time: " + datetime.now().strftime("%X"))
+    print("The current temperature is " + str(temperature_initial))
+    print("The final temperature is " + str(temperature_final))
+    print("Average Temperature: ", format(average_temperature, ".2f"))
+
+
+def main():
+    temperature_initial = get_temperature()
+    temperature_final = get_temperature_final()
+    minutes = get_minutes()
+    average_temperature = calc_average(temperature_final, temperature_initial, minutes)
+    print_all(temperature_final, temperature_initial, average_temperature)
+
+
+if __name__ == '__main__':
+    main()
